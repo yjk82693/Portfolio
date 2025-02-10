41db103503e5e18cce6e68b30f9201f25e7f2a22
Yoojun Kim <yjkxlr8tion@gmail.com>
2025-02-10 16:54:52 -0500

    edit

--- Portfolio/next.config.mjs
+++ Portfolio/next.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/Portfolio",
-    assetPrefix: "/Portfolio",
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "raw.githubusercontent.com",
-          pathname: "/yjk82693/Portfolio/main/my-portfolio/public/images/**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/yjk82693/Portfolio/main/my-portfolio/public/images/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
